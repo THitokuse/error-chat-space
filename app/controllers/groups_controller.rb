2fe898dc57f5d9028cfd4cb2744fba6019450dcb
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-11-19 14:41:09 +0900

    group作成フォームの共通化
    group
    newとeditのフォームを共通化

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,11 +23,19 @@ class GroupsController < ApplicationController
   end
 
   def update
-
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
