038c2916bf47a901e7cee22601308bd519459351
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-11-19 22:03:23 +0900

    ajax通信を動くようにする
    1.messages#createを動かす。
    2.jbuilderを使用して、作成したメッセージをJSON形式で返す。
    3.返ってきたJSONをdoneメソッドで受取り、HTMLを作成する。

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,5 +1,28 @@
 $(function() {
   $('.msg_form').on('submit', function(e){
-    console.log("イベント発火");
+    //フォームのsubmitイベントを中止
+    e.preventDefault();
+    //ajaxでリクエストを送る際のパスを取得
+    var url = window.location.pathname;
+    //フォームに入力された値を取得
+    var formData = new FormData($(this).get()[0]);
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      console.log(message);
+      $('.chat-footer__text').add('.hidden').val('');
+      $('.send').prop('disabled', false);
+    })
+    .fail(function(){
+      console.log("error");
+      $('.chat-footer__text').add('.hidden').val('');
+    })
   })
 })
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,7 @@
+json.content  @message.content
+json.image  @message.image
+json.user_name  @message.user.name
+json.date  @message.created_at.strftime('%Y/%m/%d %H:%M:%S')
+json.is_content_present  @message.content.present?
+json.is_image_present  @message.image.present?
+json.id  @message.id
