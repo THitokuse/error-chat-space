eb13e9e47d9a4666ee7af304e9ce824637898e62
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-11-19 13:42:11 +0900

    groupモデル & group_userモデルDB作成
    groupテーブル → nameカラム
    group_user中間テーブル → user_id, group_idカラム作成

--- db/migrate/20181119043850_create_groups.rb
+++ db/migrate/20181119043850_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
-
+      t.string :name, null: false, index: true, unique: true
       t.timestamps
     end
   end
