735eb0d86304811ac00f354feea93a5ee5bbfa14
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-11-20 21:41:50 +0900

    users/indexのアクション作成
    jbuilderとcontrollerのアクション作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{user_params[:user]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.html { redirect_to new_group_path}
+      format.json
+  end
+
   def edit
 
   end
