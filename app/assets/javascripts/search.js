7e972d5ee7189791d20fce8cf9a1b270a12dad98
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-11-21 13:07:42 +0900

    追加ボタンを押すとチャットメンバー追加
    追加ボタンを押すとチャットメンバーが追加される。

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -12,6 +12,20 @@ $(document).on("turbolinks:load", function() {
     return html;
   }
 
+  function appendMembers(name, user_id) {
+    var html =
+      '<div class="chat-group-user">' +
+      '<input type = "hidden", value = ' + user_id + ', name = "group[user_ids][]", id ="group_user_ids_' + user_id + '">' +
+      '<p class="chat-group-user__name">' +
+      name +
+      '</p>' +
+      '<p class="chat-group-user__btn chat-group-user__btn--remove">' +
+      '削除' +
+      '</p>' +
+      '</div>';
+    return html;
+  }
+
   $(function() {
     $(".chat-group-form__input").on("keyup", function() {
       $("#user-search-result").children().remove();
@@ -34,6 +48,18 @@ $(document).on("turbolinks:load", function() {
       .fail(function() {
         console.log('ユーザー検索に失敗しました');
       });
+    });
+  });
+
+  // ユーザーを追加
+  $(function() {
+    $("#user-search-result").on("click", ".chat-group-user__btn--add", function() {
+      var name = $(this).prev().text();
+      var user_id = $(this).data('id');
+      $(this).parent().remove();
+      var html = appendMembers(name, user_id);
+      $('#chat-group-users').append(html);
+      $(this).parent().remove();
     });
   });
 });
