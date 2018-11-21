49fcb57fd31832f052d88ce832b3798b7d207bac
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-11-21 12:47:33 +0900

    インクリメンタルサーチで検索結果表示
    入力された値に応じて検索結果を表示するHTMLをappendUserで作成

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,20 +1,40 @@
+$(document).on("turbolinks:load", function() {
+  function appendUsers(user) {
+    var html =
+      '<div class="chat-group-user">' +
+      '<p class="chat-group-user__name">' +
+      user.name +
+      '</p>' +
+      '<p class="chat-group-user__btn chat-group-user__btn--add" data-id = ' + user.id + '>' +
+      '追加' +
+      '</p>' +
+      '</div>';
+    return html;
+  }
 
-$(function() {
-  $(".chat-group-form__input").on("keyup", function() {
-    $("#user-search-result").children().remove();
-    var user = $("#user-search-field").val();
-    console.log(user);
-      $.ajax({
-        type: 'GET',
-        url: '/users',
-        data: { user: user },
-        dataType: 'json'
-      })
-    .done(function(users) {
-      console.log("成功");
-       })
-    .fail(function() {
-      console.log('ユーザー検索に失敗しました');
+  $(function() {
+    $(".chat-group-form__input").on("keyup", function() {
+      $("#user-search-result").children().remove();
+      var user = $("#user-search-field").val();
+      console.log(user);
+        $.ajax({
+          type: 'GET',
+          url: '/users',
+          data: { user: user },
+          dataType: 'json'
+        })
+      .done(function(users) {
+          $("#user_search_result").empty();
+            if (users.length !== 0) {
+              users.forEach(function(user){
+                var html = appendUsers(user);
+                $('#user-search-result').append(html);
+              })
+            }
+         })
+      .fail(function() {
+        console.log('ユーザー検索に失敗しました');
+      });
     });
   });
 });
