c67d45331266fd323e3b92fe5e7ebc2ef3f8a0bd
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-11-21 12:43:01 +0900

    インクリメンタルサーチ用のajax通信
    ajax通信ができた場合、consoleに成功とでる。

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,20 @@
-$(document).on("turbolinks:load", function() {
-  $(function() {
-    $(".chat-group-form__input").on("keyup", function() {
+
+$(function() {
+  $(".chat-group-form__input").on("keyup", function() {
+    $("#user-search-result").children().remove();
+    var user = $("#user-search-field").val();
+    console.log(user);
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { user: user },
+        dataType: 'json'
+      })
+    .done(function(users) {
       console.log("成功");
-    })
-  })
-})
+       })
+    .fail(function() {
+      console.log('ユーザー検索に失敗しました');
+    });
+  });
+});
