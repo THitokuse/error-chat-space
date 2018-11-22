ea7ddf26ca0d7abce20a1d9cbc5a8cca72c4bf89
TaichiroHasegawa <hasegawa.hitokuse@gmail.com>
2018-11-22 17:11:51 +0900

    timezone表示の変更！
    message.created_at.strftime('%Y/%m/%d %H:%M:%S') →　message.created_at.to_s(:default)
    
    timezoneの表示をTokyoに変更

--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,4 +2,4 @@ json.id  @message.id
 json.user_name  @message.user.name
 json.content  @message.content
 json.image  @message.image.url
-json.date  @message.created_at.to_s
+json.date  @message.created_at.to_s(:default)
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,4 @@
-Time::DATE_FORMATS[:default] = '%Y/%m/%d %H:%M'
-Time::DATE_FORMATS[:datetime] = '%Y/%m/%d %H:%M'
+Time::DATE_FORMATS[:default] = '%Y/%m/%d %H:%M:%S'
+Time::DATE_FORMATS[:datetime] = '%Y/%m/%d %H:%M:%S'
 Time::DATE_FORMATS[:date] = '%Y/%m/%d'
 Time::DATE_FORMATS[:time] = '%H:%M:%S'
-Date::DATE_FORMATS[:default] = '%Y/%m/%d'
